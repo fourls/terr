550956669fa6010fd64206145a9dce746037e321
Elliot Hillary <elliot@peers.net.au>
2024-06-26 18:43:07 +1000

    Add configuration to terraria.sh

--- terraria.sh
+++ terraria.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "usage: run.sh [ install | init | run ]"
+    echo "usage: terraria.sh [ install | config | choose | run ]"
     exit 1
 fi
 
@@ -16,8 +16,9 @@ case $UNAME in
     *) EXE_PATH="${SERVERFILES}/TerrariaServer.bin.x86_64";;
 esac
 
-function install_server() {
+function install_server {
     local version=$1
+    shift
 
     if [ "$UNAME" == Darwin ]; then
         local platform="Mac"
@@ -41,32 +42,86 @@ function install_server() {
     chmod u+x "${EXE_PATH}"
     echo "Configured permissions for ${EXE_PATH}"
 
-    # shellcheck disable=SC2005
-    echo "$(cat <<EOF
-worldpath=${WORLDPATH}
-password=
-EOF
-    )" > "$CONFIG"
+    configure_server -port 7777 -discard
 
     rm -rf working
 }
 
-function run_terraria() {
+function configure_server {
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -port) shift && local port=$1;;
+            -password) shift && local password=$1;;
+            -maxplayers) shift && local maxplayers=$1;;
+            -motd) shift && local motd=$1;;
+            -discard) local discard=1;;
+            *) echo "Unknown install parameter '$1'; ignoring.";;
+        esac
+
+        shift
+    done
+
+    if [ -z "$discard" ]; then
+        while read -r line; do
+            IFS="=" read -ra split <<< "$line"
+            local key="${split[0]}"
+            local value="${split[1]}"
+
+            if [ -n "$value" ]; then
+                case $key in
+                    port) [ -z "$port" ] && local port=$value;;
+                    password) [ -z "$password" ] && local password=$value;;
+                    maxplayers) [ -z "$maxplayers" ] && local maxplayers=$value;;
+                    motd) [ -z "$motd" ] && local motd=$value;;
+                esac
+            fi
+        done < "$CONFIG"
+    fi
+
+    local settings
+    settings="$(cat <<EOF
+# Non-configurable settings
+worldpath=${WORLDPATH}
+# Configurable settings
+port=${port}
+password=${password}
+maxplayers=${maxplayers}
+motd=${motd}
+EOF
+    )" || true
+
+    echo "$settings"
+    echo "$settings" > "$CONFIG"
+}
+
+function run_terraria {
     "$EXE_PATH" "$@"
 }
 
-case $1 in
-    install) install_server "1449" ;;
-    choose) run_terraria -config "$CONFIG" ;;
-    run)
-        if [ -z "$2" ]; then
-            echo "usage: run.sh run <world name>" && exit 1
-        elif [ ! -f "${WORLDPATH}/${2}.wld" ]; then
-            echo "World '${2}' does not exist."
-            exit 2
-        else
-            run_terraria -config "$CONFIG" -world "${WORLDPATH}/${2}.wld"
-        fi
-    ;;
-    *) echo "unknown command '$1'" && exit 1 ;;
-esac
+function run_terraria_world {
+    local worldname=$1 && shift
+    local worldfile="${WORLDPATH}/${worldname}.wld"
+
+    if [ -z "$worldname" ]; then
+        echo "usage: terraria.sh run <world name>" && exit 1
+    elif [ ! -f "$worldfile" ]; then
+        echo "World '${worldname}' does not exist."
+        exit 2
+    fi
+
+    run_terraria -config "$CONFIG" -world "$worldfile" "$@"
+}
+
+function main {
+    local cmd=$1 && shift
+
+    case $cmd in
+        install) install_server "1449" "$@" ;;
+        config) configure_server "$@" ;;
+        choose) run_terraria -config "$CONFIG" ;;
+        run) run_terraria_world "$@" ;;
+        *) echo "unknown command '$cmd'" && exit 1 ;;
+    esac
+}
+
+main "$@"
